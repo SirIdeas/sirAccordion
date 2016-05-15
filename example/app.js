10c25d4446c883bb7c94b9d9ca0f72474544f5f0
Axel Salmeron <axel.salmeron@gmail.com>
2016-05-15 12:59:05 -0400

    Updating example
    writting a better example page for the next accordion version

--- example/app.js
+++ example/app.js
@@ -25,7 +25,7 @@ app.controller('Principal',['$scope','$compile',function($scope,$compile){
     afterHeader: '',
     topContentClass: '',
     beforeTopContent: '',
-    afterTopContent: '<div><strong>this is the afterTopContent attr</strong></div>',
+    afterTopContent: '<p><button class="btn btn-secondary"><strong>Back to top</strong></button></p>',
     bottomContentClass: '',
     beforeBottomContent: '',
     afterBottomContent: ''
@@ -33,24 +33,28 @@ app.controller('Principal',['$scope','$compile',function($scope,$compile){
 
   $scope.accordionArray = 
   [
-    {"title":"Level 1","topContent":"This is the top content attr","bottomContent":null,"subCollection":[
-      {"title":"This is a Level 2 Header!","topContent":"This is some nice text right here","bottomContent":null},
-      {"title":"Level 2","topContent":"<pre>And you can ad Html code directly to the content too</pre>","bottomContent":null},
-      {"title":"This Level 2 Header has another subCollection","topContent":null,"bottomContent":"This is the bottom content attr :)","subCollection":[
-        {"title":"And You got to the Level 3","topContent":"<span>Awesome text or HTML Content Here</span>"},
-        {"title":"Level 3","topContent":"You can also add AngularJS expressions","subCollection":[
-          {"title":"And another Level, Level 4","topContent":"<p>Awesome text or HTML Content Here</p>"},
-          {"title":"Level 4","topContent":"<p>Adding HTML</p><div style=\"display:inline-block;width:30%;height:100px;background:blue;margin:5px;padding:5px;\"></div><div style=\"display:inline-block;width:30%;height:100px;background:red;margin:5px;padding:5px;\"></div>"}
+    {
+      "title": "Sir accordion",
+      "topContent": "",
+      "bottomContent": "",
+      "subCollection": [
+        {"title": "Introduction", "topContent": "<div ng-include=\"'accordionContent/top/1-1.html'\"></div>"},
+        {"title": "The collection", "topContent": "<div ng-include=\"'accordionContent/top/1-2.html'\"></div>", "subCollection": [
+          {"title": "Title", "topContent": "<p>The <code>title</code> attr sets the content of the header in each item.</p>"},
+          {"title": "topContent", "topContent": "<div ng-include=\"'accordionContent/top/1-2-2.html'\"></div>"},
+          {"title": "bottomContent", "topContent": "<div ng-include=\"'accordionContent/top/1-2-3.html'\"></div>"},
+          {"title": "subCollection", "topContent": "<div ng-include=\"'accordionContent/top/1-2-4.html'\"></div>"},
         ]},
-        {"title":"Level 3","topContent":"<p>Awesome text or HTML Content Here</p>"}
-      ]},
-      {"title":"Level 2","topContent":"<p>You can keep adding Levels</p>","bottomContent":null, "subCollection":[
-        {"title":"Level 3","topContent":"<p>You can keep adding Levels</p>","bottomContent":null, "subCollection":[
-          {"title":"Level 4","topContent":"<p>You can keep adding Levels</p>","bottomContent":null}
+        {"title":"The subCollection","topContent":null,"bottomContent":"This is the bottom content attr :)","subCollection":[
+        ]},
+        {"title":"As many levels as you want","topContent":"<p>You can keep adding Levels</p>","bottomContent":null, "subCollection":[
+          {"title":"Level 3","topContent":"<p>You can keep adding Levels</p>","bottomContent":null, "subCollection":[
+            {"title":"Level 4","topContent":"<p>You can keep adding Levels</p>","bottomContent":null}
+          ]}
         ]}
-      ]}
-    ]},
-    {"title":"Level 1","topContent":null,"bottomContent":null,"subCollection":[
+      ]
+    },
+    {"title":"Configuration object","topContent":null,"bottomContent":null,"subCollection":[
       {"title":"Level 2","topContent":"<p>You Can set a Content, a subCollection of items, or combine them; making an element expand to a collection of items with text on the top, bottom or both!.</p>\n<p>Check the next items for examples</p>","bottomContent":null},
       {"title":"Level 2","topContent":"This is a top content","bottomContent":"This text should be the content after the Items","subCollection":[
         {"title":"Level 3 item","topContent":"Awesome text"},
@@ -58,23 +62,18 @@ app.controller('Principal',['$scope','$compile',function($scope,$compile){
       ]},
       {"title":"Level 2","topContent":"<h3>sir-accordion is also perfect for responsive Design!</h3>\n<strong>The content uses VelocityJS, and you can resize the window and it will adapt pretty well</strong>\n<p>You can try it out by using the content provided or adding your own content and resizing your desktop window, it's awesome</p>","bottomContent":null}
     ]},
-    {"title":"Level 1","topContent":"This one only goes to Level 1 and the content in it and this is the topContent","bottomContent":"this is the bottom content"},
-    {"title":"Level 1","topContent":"This is the topContent field on this element and it also has an array of items (subCollection array field in the JSON object)","bottomContent":null,"subCollection":[
+    {"title":"Events","topContent":"This one only goes to Level 1 and the content in it and this is the topContent","bottomContent":"this is the bottom content"},
+    {"title":"Themes","topContent":"This is the topContent field on this element and it also has an array of items (subCollection array field in the JSON object)","bottomContent":null,"subCollection":[
       {"title":"Level 2","topContent":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.","bottomContent":null},
       {"title":"Level 2","topContent":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.","bottomContent":null},
       {"title":"Level 2","topContent":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.","bottomContent":null}
     ]},
-    {"title":"Level 1","topContent":null,"bottomContent":null,"subCollection":[
+    {"title":"Advanced","topContent":null,"bottomContent":null,"subCollection":[
       {"title":"Level 2","topContent":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.","bottomContent":null},
       {"title":"Level 2","topContent":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.","bottomContent":null},
       {"title":"Level 2","topContent":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.","bottomContent":null}
     ]},
-    {"title":"Level 1","topContent":null,"bottomContent":null,"subCollection":[
-      {"title":"Level 2","topContent":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.","bottomContent":null},
-      {"title":"Level 2","topContent":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.","bottomContent":null},
-      {"title":"Level 2","topContent":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.","bottomContent":null}
-    ]},
-    {"title":"Level 1","topContent":null,"bottomContent":null,"subCollection":[
+    {"title":"Contributing","topContent":null,"bottomContent":null,"subCollection":[
       {"title":"Level 2","topContent":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.","bottomContent":null},
       {"title":"Level 2","topContent":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.","bottomContent":null},
       {"title":"Level 2","topContent":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.","bottomContent":null}
@@ -86,7 +85,12 @@ app.controller('Principal',['$scope','$compile',function($scope,$compile){
     $scope.accordionConfig.autoCollapse = !$scope.accordionConfig.autoCollapse;
   };
 
-  $scope.expandByCoord = function(){
+  $scope.expandByCoord = function(coord){
+    console.log(coord);
+    if(coord){
+      $scope.$broadcast('sacExpandContentById', coord);
+      return
+    }
     $scope.$broadcast('sacExpandContentById', $scope.coord);
   }
 
